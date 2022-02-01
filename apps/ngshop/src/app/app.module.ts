d40a49e7b2ba514b70b594e006c41fa8c0ec7b85
leonero2807 <trungpham280797@gmail.com>
2022-02-02 01:16:10 +0700

    styling structure

---
+++ apps/ngshop/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@bluebits/ui';
+
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'products', component: ProductListComponent }
+];
+
+@NgModule({
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    projects: ['<rootDir>/apps/ngshop', '<rootDir>/apps/admin', '<rootDir>/libs/ui', '<rootDir>/libs/products', '<rootDir>/libs/users', '<rootDir>/libs/orders']
+};
